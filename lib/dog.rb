59e61bc81b367b20de3fa26ae2800ffb878ff2f2
alex tanui <tanuialex417@gmail.com>
2023-02-10 09:41:44 +0300

    completed assignment

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,19 @@
 class Dog
-
+    def bark
+        puts "Woof!"
+      end
+    def sit
+        puts 'The Dog is sitting'
+    end
 end
+
+fino = Dog.newclass Dog
+    def bark
+        puts "Woof!"
+      end
+    def sit
+        puts 'The Dog is sitting'
+    end
+end
+
+fino = Dog.new
